ed152ec2adca431ff272c437c503145ed83d0812
abhaianand <abhaianand6@gmail.com>
2024-11-11 01:17:55 +0530

    g

--- java_interview_project/Right_angle_triangle.java
+++ java_interview_project/Right_angle_triangle.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Right_angle_triangle {
+public class Right_angle_triangle {
+
+    public static void main(String args[]) {
+        int loopcount=10;
+        for(int i=1;i<=loopcount;i++){
+
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
--- java_interview_project/Triangle_star.java
+++ java_interview_project/Triangle_star.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Triangle_star {
+public class Triangle_star {
+    public static void main(String args[]) {
+        int level = 6;
+        String space = " ";
+        String star = "*";
+        for (int i = 1; i<= level;i++) {
+            for (int p = 1; p <= 7 - i; p++) {
+                System.out.print(" ");
+            }
+            for (int r =1;r<=i*2-1;r++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
+
+
+     /*   123456*
+                12345***
+        1234*****
+        123*******
+        12*********
+        1***********
+       *************
+
+        space(p) star(r) loop(i)   relation of r  relation of p
+        6          1       1          r=i*2-1        p =7-i
+        5          3       2
+        4          5       3
+        3      	   7       4
+        2          9       5
+        1          11      6*/
